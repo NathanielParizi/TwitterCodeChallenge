ade3f938bc00ef897d89d03ee2d64e3b7d91d53c
Joel Parizi <joelparizi@Joels-MBP.attlocal.net>
2021-03-15 21:33:49 -0400

    second commit

--- app/src/main/java/com/example/twittercodechallenge/MainActivity.kt
+++ app/src/main/java/com/example/twittercodechallenge/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.twittercodechallenge
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.twittercodechallenge.databinding.ActivityMainBinding
+
+private var temperature = 0
 
 private lateinit var binding: ActivityMainBinding
 
@@ -13,5 +16,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.temperature.text = TemperatureConverter.celsiusToFahrenheit(34F).toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+
+        outState.putInt("key", temperature);
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        temperature = savedInstanceState.getInt("key", 0)
+        binding.temperature.text = TemperatureConverter.celsiusToFahrenheit(temperature.toFloat()).toString()
     }
 }
